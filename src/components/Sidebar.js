123c599a27d3760ec6fbf49c084ed138406f64a6
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-17 01:25:58 +0530

    added history section with icon in subscription

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,62 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  const isMenuOpen=useSelector((store)=>store.app.isMenuOpen);
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
 
-  if(!isMenuOpen)return null;
+  if (!isMenuOpen) return null;
   return (
-    <div className='flex flex-col w-48 p-5 shadow-lg '>
-        <ul className=''>
-         <li><Link to="/">Home</Link></li>
-            <li><Link to="shorts">Shorts</Link></li>
-            <li>Videos</li>
-            <li>Live</li>
-            <li>History</li>
-        </ul>
-        <h1 className='text-2xl font-bold'>Subscriptions</h1>
-        <ul className=''>
-            <li>Music</li>
-            <li>Sports</li>
-            <li>Gaming</li>
-            <li>Movies</li>
-        </ul>
-        
-        <h1 className='text-2xl font-bold pt-7'>Watch Later</h1>
-        <ul>
-            <li>Music</li>
-            <li>Sports</li>
-            <li>Gaming</li>
-            <li>Movies</li>
-        </ul>
-    </div>
-  )
-}
+    <div className="flex flex-col w-48 p-5 shadow-lg ">
+      <ul className="">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="shorts">Shorts</Link>
+        </li>
+        <li>Videos</li>
+        <li>Live</li>
+        <li>History</li>
+      </ul>
+      <h1 className="text-2xl font-bold">Subscriptions</h1>
+      <ul className="">
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+        <li>
+          <span className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-history"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 8l0 4l2 2" />
+              <path d="M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5" />
+            </svg>
+            <span className="ml-1">History</span>
+          </span>
+        </li>
+      </ul>
 
-export default Sidebar
+      <h1 className="text-2xl font-bold pt-7">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Sports</li>
+        <li>Gaming</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
